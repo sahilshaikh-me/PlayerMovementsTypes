f914b527f8b6422242dfac4943fc200e13130961
sahil shaikh <50543009+sahilshaikh-me@users.noreply.github.com>
2024-04-11 18:31:50 +0530

    Update Player.cs

--- ThirdPersonPerspective/Player.cs
+++ ThirdPersonPerspective/Player.cs
@@ -1,84 +1,95 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-//Attach This Script to Player
 public class Player : MonoBehaviour
 {
-
     public static Player Instance { get; set; }
+
     #region Fpp Variable
-    public float MoveSpeed = 3;
+    public float MoveSpeed = 3f;
+    public float SprintSpeedMultiplier = 2f;
     public Rigidbody rb;
     public float Vertical;
     public float Horizontal;
+    public Vector2 inputDir;
     #endregion
 
-    #region Tpp Vriable
+    #region Tpp Variable
     public float turnSmoothTime = 0.2f;
     float turnSmoothVelocity;
-
-    public float speedSmoothTime = 0.1f;
-    float speedDmoothVelocity;
-    Transform cameraT;
-   //CharacterController controller; fix while roatating player moving forward
-    #endregion
     public bool isFpp;
+    Transform cameraT;
+    #endregion
+
+    #region Jump Variable
+    public float jumpForce = 8f;
+    public bool isGrounded;
+    #endregion
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         cameraT = Camera.main.transform;
-         controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        if (isFpp == true)
+        if (isFpp)
         {
             Horizontal = Input.GetAxis("Horizontal");
             Vertical = Input.GetAxis("Vertical");
-            Vector3 Movement = new Vector3(Horizontal, 0, Vertical) * MoveSpeed * Time.deltaTime;
+            Vector3 Movement = new Vector3(Horizontal, 0f, Vertical) * MoveSpeed * Time.deltaTime;
+
+            // Sprint logic
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                Movement *= SprintSpeedMultiplier;
+            }
 
             transform.Translate(Movement);
         }
         else
         {
-            // Only move if not in FPP mode
-            Vector2 inputt = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-            Vector2 inputDir = inputt.normalized;
+            inputDir = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
 
             if (inputDir != Vector2.zero)
             {
                 float targetRotation = Mathf.Atan2(inputDir.x, inputDir.y) * Mathf.Rad2Deg + cameraT.eulerAngles.y;
                 transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, turnSmoothTime);
-                transform.Translate(transform.forward * MoveSpeed * Time.deltaTime, Space.World);
 
+                // Calculate movement direction based on input without relying on the forward direction
+                Vector3 moveDir = Quaternion.Euler(0f, targetRotation, 0f) * Vector3.forward;
+                float moveSpeed = MoveSpeed;
+
+                // Sprint logic
+                if (Input.GetKey(KeyCode.LeftShift))
+                {
+                    moveSpeed *= SprintSpeedMultiplier;
+                }
+
+                transform.Translate(moveDir * moveSpeed * Time.deltaTime, Space.World);
             }
-            //Fix Movements and rotations
- //float horizontal = Input.GetAxis("Horizontal");
- //float vertical = Input.GetAxis("Vertical");
- // inputDir = new Vector3(horizontal, 0f, vertical).normalized;
+        }
 
- //if (inputDir.magnitude >= 0.1f)
- //{
- //    float targetRotation = Mathf.Atan2(inputDir.x, inputDir.z) * Mathf.Rad2Deg + cameraT.eulerAngles.y;
- //    transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, turnSmoothTime);
+        // Jumping logic
+        if (!isFpp && Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Jump();
+        }
+    }
 
-  //   Vector3 moveDir = Quaternion.Euler(0f, targetRotation, 0f) * Vector3.forward;
-  //   controller.Move(moveDir.normalized * moveSpeed * Time.deltaTime);
+    void Jump()
+    {
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
 
-    // animator.SetFloat("Speed", moveDir.magnitude);
- //}
-// else
-// {
-    // animator.SetFloat("Speed", 0f);
-// }
-          
-
-            // Only move if not in FPP mode
+    void FixedUpdate()
+    {
+        if (!isFpp)
+        {
+            // Ground detection logic
+            isGrounded = Physics.Raycast(transform.position, Vector3.down, 1f);
         }
     }
 }
